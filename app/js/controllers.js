c81d5f95842b8bd291f486c048eb557f1ca0b1ce
Ádám Gólya <adam.stork@gmail.com>
2014-04-16 15:59:31 +0200

    Remove unnecessary comment

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,6 @@
 /* Controllers */
 
 FlickrSearch.controller('PhotoCtrl', function ($scope, flickrPhotos) {
-    //$scope.photos = flickrPhotos.load({ tags: "dogs" })
     $scope.search = function() {
         $scope.photos = flickrPhotos.load({ tags: $scope.keywords })
     }
